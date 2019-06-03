df8fadcf29477b6254d32c2df644b367832a3348
Huy Do <huyddo40@gmail.com>
2019-06-03 02:00:34 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,23 @@
+require "pry"
 def reformat_languages(languages)
   # your code here
+  hash = {}
+  styles =[]
+  languages.each do |key,value|
+    # hash[key]=value
+
+    styles << key
+    # binding.pry
+    value.each do |k,v|
+      hash[k] = v
+      # binding.pry
+      # styles << key
+      # hash[k][:style] = styles
+      hash[:style] = styles
+      # binding.pry
+    end
+
+  end
+  hash
+  binding.pry
 end
