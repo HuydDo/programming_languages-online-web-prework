d57e625e5317fedfd52c0a8896ae454023dbd996
Huy Do <huyddo40@gmail.com>
2019-06-03 02:16:52 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,16 +1,13 @@
 require "pry"
 def reformat_languages(languages)
-  # your code here
   hash = {}
   styles =[]
   languages.each do |key,value|
-    # hash[key]=value
     styles << key
     puts "#{styles}"
     value.each do |k,v|
       hash[k] = v
       	hash[k][:style] = styles
-		  # hash[:style] = styles
     end
   end
   hash
